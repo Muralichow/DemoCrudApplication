39b9d5bbad973d2df7525a24a0164525e9428d24
Muralichow <krishnathatha22@gmail.com>
2022-10-24 18:27:03 +0530

    changes

--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Employee;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends CrudRepository<Employee,Long> {
 
 }
